2b5d4fc15c4f23aa5ab01222ae04465fb3ca36b6
Ignacio Ambia <ignacio.ambia@gmail.com>
2024-04-02 21:16:32 -0600

    feat(pacientes/nuevo): add patient input fields

--- app/pacientes/nuevo/page.tsx
+++ app/pacientes/nuevo/page.tsx
@@ -1,5 +1,96 @@
+"use client";
+
+import { Button, Card, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField } from "@mui/material";
+import { useState } from "react";
+import { DateField } from '@mui/x-date-pickers/DateField';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import "./nuevo.scss";
 export default function NewPatient(){
- return <div>
-  Create a new patient here
+
+ const [ inputs, setInputs] = useState<Partial<NewPatient>>({});
+ const handleChange = (event: any) => {
+  const name = event.target.name;
+  const value = event.target.value;
+  setInputs((values: any) => ({...values, [name]: value}))
+ }
+
+ const addNewPatient =  () => {
+  console.log('New patient info is: ', inputs);
+ };
+ return <div className="new-patient-wrapper">
+  <h1>Crear nuevo paciente</h1>
+  <Card className="new-patient-card">
+  <TextField
+            value={inputs.name}
+            onChange={handleChange}
+            className="field"
+            label="Nombre(s)"
+            name-="name"
+            autoComplete="on"
+            variant="outlined"
+          />
+                     <TextField
+            value={inputs.lastName}
+            onChange={handleChange}
+            className="field"
+            label="Apellido Paterno"
+            name="lastName"
+            autoComplete="on"
+            variant="outlined"
+          />
+                     <TextField
+            value={inputs.mothersLastName}
+            onChange={handleChange}
+            className="field"
+            name="mothersLastName"
+            label="Apellido Materno"
+            autoComplete="on"
+            variant="outlined"
+          />
+
+<LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateField label="Nacimiento(mm/dd/aaaa)"  disableFuture />
+    </LocalizationProvider>
+
+<TextField
+
+            value={inputs.address}
+            onChange={handleChange}
+            className="field col-2"
+            label="Dirección"
+            name="address"
+            autoComplete="on"
+            variant="outlined"
+          />
+<TextField
+            value={inputs.email}
+            onChange={handleChange}
+            className="field col-2"
+            label="Email"
+            name="email"
+            autoComplete="on"
+            variant="outlined"
+          />
+
+
+<FormControl>
+  <FormLabel id="demo-radio-buttons-group-label">Género</FormLabel>
+  <RadioGroup
+    aria-labelledby="demo-radio-buttons-group-label"
+    defaultValue="female"
+    name="radio-buttons-group"
+  >
+    <FormControlLabel value="female" control={<Radio />} label="Femenino" />
+    <FormControlLabel value="male" control={<Radio />} label="Masculino" />
+  </RadioGroup>
+</FormControl>
+  </Card>
+
+  <div className="btn-wrapper">
+   <Button onClick={addNewPatient} variant="contained">Crear paciente</Button>
+  </div>
+
+
  </div>
 }
---
+++ app/pacientes/nuevo/types/NewPatient.type.ts
@@ -0,0 +1,9 @@
+interface NewPatient {
+ name: string;
+ lastName?: string;
+ mothersLastName?: string;
+ gender: any;
+ address: string;
+ birthday: any;
+ email?: string;
+}
